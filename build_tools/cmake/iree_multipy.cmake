operskoj2c/iree

commit c8f906cb4558204bffe385bbf1bbebba04302186

Step 2 of enabling multipy support. (#2590)
* Adds a script to install deps and get configure args for all installed python versions.
* Makes iree_py_test() multipy aware so that it runs a test for every version.
* With this, extensions build for all python versions via the dockcross-manylinux2014 images. (tests are failing on an unrelated lld problem).

build_tools/cmake/iree_multipy.cmake
@@ -22,14 +22,17 @@ function(iree_multipy_configure)
   # Configure the defaults.
   # Note that this is using the pybind11 configuration vars, which creates
   # a fragile dependency. It would be better to derive these locally.
-  set(IREE_MULTIPY_DEFAULT_INCLUDE_DIRS "${PYTHON_INCLUDE_DIRS}" CACHE INTERNAL "Python include dirs" )
-  set(IREE_MULTIPY_DEFAULT_LIBRARIES "${PYTHON_LIBRARIES}" CACHE INTERNAL "Python libraries")
-  set(IREE_MULTIPY_DEFAULT_PREFIX "${PYTHON_MODULE_PREFIX}" CACHE INTERNAL "Python module prefix")
-  set(IREE_MULTIPY_DEFAULT_SUFFIX "${PYTHON_MODULE_SUFFIX}" CACHE INTERNAL "Python module suffix")
-  set(IREE_MULTIPY_DEFAULT_EXTENSION "${PYTHON_MODULE_EXTENSION}" CACHE INTERNAL "Python module extension")
+  if(PYTHONLIBS_FOUND)
+    set(IREE_MULTIPY_DEFAULT_EXECUTABLE "${PYTHON_EXECUTABLE}" CACHE INTERNAL "Python executable" )
+    set(IREE_MULTIPY_DEFAULT_INCLUDE_DIRS "${PYTHON_INCLUDE_DIRS}" CACHE INTERNAL "Python include dirs" )
+    set(IREE_MULTIPY_DEFAULT_LIBRARIES "${PYTHON_LIBRARIES}" CACHE INTERNAL "Python libraries")
+    set(IREE_MULTIPY_DEFAULT_PREFIX "${PYTHON_MODULE_PREFIX}" CACHE INTERNAL "Python module prefix")
+    set(IREE_MULTIPY_DEFAULT_SUFFIX "${PYTHON_MODULE_SUFFIX}" CACHE INTERNAL "Python module suffix")
+    set(IREE_MULTIPY_DEFAULT_EXTENSION "${PYTHON_MODULE_EXTENSION}" CACHE INTERNAL "Python module extension")
+  endif()
 
   if(IREE_MULTIPY_VERSIONS)
-    set(IREE_MULTIPY_VERSIONS_EFFECTIVE "${IREE_MULTIPY_VERSIONS}")
+    set(IREE_MULTIPY_VERSIONS_EFFECTIVE "${IREE_MULTIPY_VERSIONS}" CACHE INTERNAL "Python extension versions")
   else()
     message(STATUS "Multi-python extension versions not found: using defaults")
     set(IREE_MULTIPY_VERSIONS_EFFECTIVE "DEFAULT" CACHE INTERNAL "Python extension versions")
@@ -39,18 +42,22 @@ function(iree_multipy_configure)
   message(STATUS "Multipy extension versions: ${IREE_MULTIPY_VERSIONS_EFFECTIVE}")
   foreach(V ${IREE_MULTIPY_VERSIONS_EFFECTIVE})
     message(STATUS "  - Multipy version ${V}")
+    message(STATUS "    : EXECUTABLE = ${IREE_MULTIPY_${V}_EXECUTABLE}")
     message(STATUS "    : INCLUDE_DIRS = ${IREE_MULTIPY_${V}_INCLUDE_DIRS}")
     message(STATUS "    : LIBRARIES = ${IREE_MULTIPY_${V}_LIBRARIES}")
     message(STATUS "    : PREFIX = ${IREE_MULTIPY_${V}_PREFIX}")
     message(STATUS "    : SUFFIX = ${IREE_MULTIPY_${V}_SUFFIX}")
     message(STATUS "    : EXTENSION = ${IREE_MULTIPY_${V}_EXTENSION}")
 
-    # Only INCLUDE_DIRS and EXTENSION are needed for all configs.
+    # Check for required settings.
     if(NOT IREE_MULTIPY_${V}_INCLUDE_DIRS)
-      message(FATAL "MULTIPY config ${V}: No IREE_MULTIPY_{VER}_INCLUDE_DIRS var")
+      message(FATAL " MULTIPY version ${V}: No IREE_MULTIPY_${VER}_EXECUTABLE var")
+    endif()
+    if(NOT IREE_MULTIPY_${V}_INCLUDE_DIRS)
+      message(FATAL " MULTIPY version ${V}: No IREE_MULTIPY_${VER}_INCLUDE_DIRS var")
     endif()
     if(NOT IREE_MULTIPY_${V}_EXTENSION)
-      message(FATAL "MULTIPY config ${V}: No IREE_MULTIPY_{VER}_EXTENSION var")
+      message(FATAL " MULTIPY version ${V}: No IREE_MULTIPY_${VER}_EXTENSION var")
     endif()
   endforeach()
 endfunction()
@@ -132,10 +139,10 @@ function(iree_pyext_module)
     # Track target and deps, use in iree_complete_py_extension_link_options() later.
     # See iree_complete_py_extension_link_options() in iree_py_extension.cmake
     # TODO: Move that implementation here.
-    list(TRANSFORM ARG_PYEXT_DEPS APPEND "__${V}")
+    set(TRANSFORMED_PYEXT_DEPS "${ARG_PYEXT_DEPS}")
+    list(TRANSFORM TRANSFORMED_PYEXT_DEPS APPEND "__${V}")
     set_property(GLOBAL APPEND PROPERTY _IREE_PY_EXTENSION_NAMES "${VER_NAME}")
-    set_property(TARGET ${VER_NAME} PROPERTY DIRECT_DEPS ${ARG_DEPS} ${ARG_PYEXT_DEPS})
-
+    set_property(TARGET ${VER_NAME} PROPERTY DIRECT_DEPS ${ARG_DEPS} ${TRANSFORMED_PYEXT_DEPS})
     _alias_iree_pyext_library("${ARG_NAME}" "${V}" ${VER_NAME})
   endforeach()
 endfunction()
@@ -167,11 +174,12 @@ function(iree_pyext_library)
         "${IREE_MULTIPY_${V}_INCLUDE_DIRS}"
         "$<BUILD_INTERFACE:${IREE_COMMON_INCLUDE_DIRS}>"
     )
-    list(TRANSFORM ARG_PYEXT_DEPS APPEND "__${V}")
+    set(TRANSFORMED_PYEXT_DEPS "${ARG_PYEXT_DEPS}")
+    list(TRANSFORM TRANSFORMED_PYEXT_DEPS APPEND "__${V}")
     target_link_libraries(${VER_NAME}
       PUBLIC
         ${ARG_DEPS}
-        ${ARG_PYEXT_DEPS}
+        ${TRANSFORMED_PYEXT_DEPS}
       PRIVATE
         ${IREE_DEFAULT_LINKOPTS}
     )
@@ -248,6 +256,52 @@ function(iree_pyext_pybind11_options name)
     ${name} PROPERTIES CXX_VISIBILITY_PRESET "hidden")
 endfunction()
 
+# iree_py_test()
+#
+# CMake function to imitate Bazel's iree_py_test rule.
+#
+# Parameters:
+# NAME: name of test
+# SRCS: List of source file
+# DEPS: List of deps the test requires
+# LABELS: Additional labels to apply to the test. The package path is added
+#     automatically.
+
+function(iree_py_test)
+  if(NOT IREE_BUILD_TESTS)
+    return()
+  endif()
+
+  cmake_parse_arguments(
+    _RULE
+    ""
+    "NAME"
+    "SRCS;DEPS;LABELS"
+    ${ARGN}
+  )
+
+  iree_package_name(_PACKAGE_NAME)
+  set(_NAME "${_PACKAGE_NAME}_${_RULE_NAME}")
+
+  iree_package_ns(_PACKAGE_NS)
+  string(REPLACE "::" "/" _PACKAGE_PATH ${_PACKAGE_NS})
+  set(_NAME_PATH "${_PACKAGE_PATH}:${_RULE_NAME}")
+  list(APPEND _RULE_LABELS "${_PACKAGE_PATH}")
+
+  foreach(V ${IREE_MULTIPY_VERSIONS_EFFECTIVE})
+    set(VER_NAME "${_NAME_PATH}__${V}")
+    add_test(
+      NAME ${VER_NAME}
+      COMMAND ${CMAKE_SOURCE_DIR}/build_tools/cmake/run_test.sh "${IREE_MULTIPY_${V}_EXECUTABLE}" "${CMAKE_CURRENT_SOURCE_DIR}/${_RULE_SRCS}"
+      WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    )
+
+    set_property(TEST ${VER_NAME} PROPERTY LABELS "${_RULE_LABELS}")
+    set_property(TEST ${VER_NAME} PROPERTY ENVIRONMENT "PYTHONPATH=${CMAKE_BINARY_DIR}/bindings/python:$ENV{PYTHONPATH};TEST_TMPDIR=${_NAME}_${V}_test_tmpdir")
+    # TODO(marbre): Find out how to add deps to tests.
+    #               Similar to _RULE_DATA in iree_lit_test().
+  endforeach()
+endfunction()
 
 ###############################################################################
 # Always-link/transitive dependency management
@@ -378,7 +432,6 @@ function(iree_complete_py_extension_link_options)
           ${_STANDARD_DEPS}
         PRIVATE
           ${_RULE_LINKOPTS}
-          ${PYTHON_LIBRARY}
       )
     else()
       target_link_libraries(${_NAME}
@@ -389,7 +442,6 @@ function(iree_complete_py_extension_link_options)
           ${_STANDARD_DEPS}
         PRIVATE
           ${_RULE_LINKOPTS}
-          ${PYTHON_LIBRARY}
       )
     endif()
   endforeach(_NAME)

CMakeLists.txt
@@ -202,7 +202,6 @@ include(iree_tablegen_doc)
 include(iree_cc_embed_data)
 include(iree_bytecode_module)
 include(iree_multipy)
-include(iree_py_test)
 include(iree_lit_test)
 include(iree_add_all_subdirs)
 include(iree_check_test)
@@ -325,7 +324,8 @@ if(${IREE_BUILD_COMPILER} OR ${IREE_BUILD_PYTHON_BINDINGS})
   find_package(PythonInterp 3 REQUIRED)
 endif()
 if(${IREE_BUILD_PYTHON_BINDINGS})
-  find_package(PythonLibs 3 REQUIRED)
+  # Note: Optional because python libs can be manually specified.
+  find_package(PythonLibs 3)
 endif()
 
 list(APPEND CMAKE_MODULE_PATH

bindings/python/build_tools/cmake/iree_py_test.cmake
@@ -1,60 +1,0 @@
-# Copyright 2020 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-include(CMakeParseArguments)
-
-# iree_py_test()
-#
-# CMake function to imitate Bazel's iree_py_test rule.
-#
-# Parameters:
-# NAME: name of test
-# SRCS: List of source file
-# DEPS: List of deps the test requires
-# LABELS: Additional labels to apply to the test. The package path is added
-#     automatically.
-
-function(iree_py_test)
-  if(NOT IREE_BUILD_TESTS)
-    return()
-  endif()
-
-  cmake_parse_arguments(
-    _RULE
-    ""
-    "NAME"
-    "SRCS;DEPS;LABELS"
-    ${ARGN}
-  )
-
-  iree_package_name(_PACKAGE_NAME)
-  set(_NAME "${_PACKAGE_NAME}_${_RULE_NAME}")
-
-  iree_package_ns(_PACKAGE_NS)
-  string(REPLACE "::" "/" _PACKAGE_PATH ${_PACKAGE_NS})
-  set(_NAME_PATH "${_PACKAGE_PATH}:${_RULE_NAME}")
-
-  add_test(
-    NAME ${_NAME_PATH}
-    COMMAND ${CMAKE_SOURCE_DIR}/build_tools/cmake/run_test.sh ${PYTHON_EXECUTABLE} "${CMAKE_CURRENT_SOURCE_DIR}/${_RULE_SRCS}"
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-  )
-
-  list(APPEND _RULE_LABELS "${_PACKAGE_PATH}")
-  set_property(TEST ${_NAME_PATH} PROPERTY LABELS "${_RULE_LABELS}")
-  set_property(TEST ${_NAME_PATH} PROPERTY ENVIRONMENT "PYTHONPATH=${CMAKE_BINARY_DIR}/bindings/python:$ENV{PYTHONPATH};TEST_TMPDIR=${_NAME}_test_tmpdir")
-  # TODO(marbre): Find out how to add deps to tests.
-  #               Similar to _RULE_DATA in iree_lit_test().
-
-endfunction()

build_tools/manylinux_py_setup.py
@@ -1,0 +1,85 @@
+#!/opt/python/cp38-cp38/bin/python3
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Generates CMake arguments to build all manylinux python versions.
+
+manylinux containers have all python version linked under /opt/python.
+This script scrapes them to get configuration, install deps, etc.
+
+Usage:
+  Install dependencies:
+    manylinux_py_setup.py deps
+  Get CMake arguments to build (typically via $() expansion):
+    manylinux_py_setup.py args
+"""
+
+import os
+from pathlib import Path
+import subprocess
+import sys
+import sysconfig
+
+
+def _get_python_exes():
+  PYTHON_PARENT_PATH = Path("/opt/python")
+  return PYTHON_PARENT_PATH.glob("*/bin/python")
+
+
+def install_deps():
+  for python_exe in _get_python_exes():
+    args = [
+        str(python_exe),
+        "-m",
+        "pip",
+        "install",
+        "absl-py",
+    ]
+    print("EXEC:", " ".join(args))
+    subprocess.check_call(args)
+
+
+def dump_current(identifier):
+  print("-DIREE_MULTIPY_{}_EXECUTABLE='{}'".format(identifier, sys.executable))
+  print("-DIREE_MULTIPY_{}_INCLUDE_DIRS='{}'".format(
+      identifier, sysconfig.get_config_var("INCLUDEPY")))
+  # TODO: Print LIBRARIES for Windows and OSX
+  print("-DIREE_MULTIPY_{}_EXTENSION='{}'".format(
+      identifier, sysconfig.get_config_var("EXT_SUFFIX")))
+
+
+def dump_all():
+  versions_ids = []
+  for python_exe in _get_python_exes():
+    identifier = python_exe.parent.parent.name
+    versions_ids.append(identifier)
+    # Invoke ourselves with a different interpreter/args to dump config.
+    subprocess.check_call(
+        [str(python_exe), __file__, "_current_args", identifier])
+  print("-DIREE_MULTIPY_VERSIONS='{}'".format(";".join(versions_ids)))
+
+
+if __name__ == "__main__":
+  if len(sys.argv) < 2:
+    print("SYNTAX: mainlinux_py_setup.py {deps|args}")
+    sys.exit(1)
+  command = sys.argv[1]
+  if command == "args":
+    dump_all()
+  elif command == "_current_args":
+    dump_current(sys.argv[2])
+  elif command == "deps":
+    install_deps()
+  else:
+    print("Unexpected command")
+    sys.exit(1)

commit 1ad2d815eb454a512e5db0ec9dc4155e0cb56665

Use ${IREE_HOST_SCRIPT_EXT} in CMake iree_py_test. (#2609)
This gets the CMake Python tests closer to running/passing on Windows. There was no regression in https://github.com/google/iree/pull/2590, the old `iree_py_test.cmake` also had this issue.

`ctest -L bindings/python -V` output before:

```
353: Test command: D:\dev\projects\iree\build_tools\cmake\run_test.sh "C:/Users/Scott/scoop/shims/python3.exe" "D:/dev/projects/iree/bindings/python/pyiree/compiler/compiler_test.py"
353: Environment variables:
353:  PYTHONPATH=D:/dev/projects/iree-build-py/bindings/python:
353:  TEST_TMPDIR=bindings_python_pyiree_compiler_compiler_test_DEFAULT_test_tmpdir
353: Test timeout computed to be: 10000000
Process not started
 D:/dev/projects/iree/build_tools/cmake/run_test.sh
[unknown error]
5/5 Test #353: bindings/python/pyiree/compiler:compiler_test__DEFAULT ...***Not Run   0.00 sec
```

`ctest -L bindings/python -V` output after:

```
353: Test command: D:\dev\projects\iree\build_tools\cmake\run_test.bat "C:/Users/Scott/scoop/shims/python3.exe" "D:/dev/projects/iree/bindings/python/pyiree/compiler/compiler_test.py"
353: Environment variables:
353:  PYTHONPATH=D:/dev/projects/iree-build-py/bindings/python:
353:  TEST_TMPDIR=bindings_python_pyiree_compiler_compiler_test_DEFAULT_test_tmpdir
353: Test timeout computed to be: 10000000
353: Running test:
353: C:/Users/Scott/scoop/shims/python3.exe D:/dev/projects/iree/bindings/python/pyiree/compiler/compiler_test.py
353: Traceback (most recent call last):
353:   File "D:/dev/projects/iree/bindings/python/pyiree/compiler/compiler_test.py", line 17, in <module>
353:     from pyiree import compiler
353: ModuleNotFoundError: No module named 'pyiree'
5/5 Test #353: bindings/python/pyiree/compiler:compiler_test__DEFAULT ...***Failed    0.44 sec
```

build_tools/cmake/iree_multipy.cmake
@@ -292,7 +292,10 @@ function(iree_py_test)
     set(VER_NAME "${_NAME_PATH}__${V}")
     add_test(
       NAME ${VER_NAME}
-      COMMAND ${CMAKE_SOURCE_DIR}/build_tools/cmake/run_test.sh "${IREE_MULTIPY_${V}_EXECUTABLE}" "${CMAKE_CURRENT_SOURCE_DIR}/${_RULE_SRCS}"
+      COMMAND
+        "${CMAKE_SOURCE_DIR}/build_tools/cmake/run_test.${IREE_HOST_SCRIPT_EXT}"
+        "${IREE_MULTIPY_${V}_EXECUTABLE}"
+        "${CMAKE_CURRENT_SOURCE_DIR}/${_RULE_SRCS}"
       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
     )
 